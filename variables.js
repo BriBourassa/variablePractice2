7e969157687a97a991dd0f987a8d1fd15f2bc6bd
Brianne Bourassa <shinybri1011@gmail.com>
2022-08-26 11:34:31 -0600

    Add variables and console logs

---
+++ variables.js
@@ -0,0 +1,14 @@
+var email = "hellogretta@gmail.com"
+var dog = "gretta"
+
+var currentAge = 14
+var outOf10 = 10
+
+var isGoodgirl = true
+var isSleeping = true
+
+var sentence = `${dog} is the goodest of girls`
+console.log(sentence)
+
+console.log(currentAge)
+console.log(isSleeping)
